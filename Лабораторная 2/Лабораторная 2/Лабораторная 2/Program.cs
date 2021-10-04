76a05cfeba9119bc6acfe85d39fa63da5d678a00
Vasilisa Kashperko <90206134+VasilisaKashperko@users.noreply.github.com>
2021-10-04 03:03:19 +0300

    Initial commit

---
+++ Лабораторная 2/Лабораторная 2/Лабораторная 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool first = true;
+            Console.WriteLine(first ? "Checked" : "Not checked");
+            Console.WriteLine(true ? "Checked" + "\n" : "Not checked"+"\n");
+
+            byte second = 255;
+            Console.WriteLine(second + " " + second.GetType());
+            Console.WriteLine(second + " " + Convert.ToInt32(second).GetType()+"\n");
+
+            object third = "Hello";
+            object fourth = 3.14;
+            Console.WriteLine($"Объект 1: {third}\nОбъект 2: {fourth}\n");
+
+            var fifth = 93;
+            fifth = 100;
+            //fifth = "Hi";
+
+            dynamic sixth = "Hi";
+            sixth = 93;
+        }
+    }
+}
